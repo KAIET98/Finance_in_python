af9f5d3499a945b5e88e9162588b4335d2e06bdf
KAIETXABET <iglesias.kaiet@opendeusto.es>
2022-08-21 16:43:43 +0200

    Construyendo los primeros ficheros industrializables

--- ANALYSIS/ETL_finance_data_building.py
+++ ANALYSIS/ETL_finance_data_building.py
@@ -1,25 +1,26 @@
 
 from modules.finantial_ratio_building.data_building_cls import FinanceRatiosBuilding
 
+data_route = 'https://assets.datacamp.com/production/repositories/1546/datasets/fb7165b7270a3721f69abf9ff09b85938d9d1068/Big9Returns2017.csv'
 
 
-def GetFinanceRatios(FinanceRatiosBuilding, finance_data_route, parse_date_option, risk_free_rate_absolute):
-    FRB = FinanceRatiosBuilding(finance_data_route= finance_data_route, 
-                                parse_date= parse_date_option, 
-                                risk_free_rate = risk_free_rate_absolute)
+FRB = FinanceRatiosBuilding(finance_data_route= data_route, 
+                            parse_date= 'yes', 
+                            risk_free_rate = 0)
 
 
-    FRB.PortfolioMarketCapitalization()
+FRB.PortfolioMarketCapitalization()
 
 
-    FRB.PortfolioVolatility()
+FRB.PortfolioVolatility()
 
 
-    FRB.SharpeRatio()
+FRB.SharpeRatio()
 
-    FRB.MSR_Portfolio()
+FRB.MSR_Portfolio()
 
 
-    data = FRB.GMV()
+data = FRB.GMV()
 
-    return data
+    
+print(data)
